ab6658e5da91533828638dbf9dd91f12a550f8d0
Durim <durim.panxha1@gmail.com>
2023-07-30 18:33:53 +0200

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,10 +28,10 @@ const RESOURCES = {"assets/1494.gif": "58fb5f753a3f7ea26eed5a44c1b3c56e",
 "icons/android-chrome-192x192.png": "aba408c1fd261b255fa16c2a8d01cd8d",
 "icons/android-chrome-512x512.png": "49f80dd3eff3ce3ee28959893bd9bbdd",
 "icons/apple-touch-icon.png": "9c5da4cdc9c922342dee6fc08822199c",
-"index.html": "116bf4280abdfe370b29268bbf686355",
-"/": "116bf4280abdfe370b29268bbf686355",
+"index.html": "01748663c5cde1f64457aa87f664214e",
+"/": "01748663c5cde1f64457aa87f664214e",
 "logo%20(2).png": "49f80dd3eff3ce3ee28959893bd9bbdd",
-"main.dart.js": "d5af346b9a559f91e976ceb4cdd95f11",
+"main.dart.js": "ed6579b152fd8feeb7d50a536569eb07",
 "manifest.json": "e0e741ef2dbc7742b7c54435b40d6c25",
 "version.json": "1991ddc54fe836f4f47c30751d5302ad"};
 // The application shell files that are downloaded before a service worker can
